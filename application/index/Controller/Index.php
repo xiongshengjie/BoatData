d4114f401513281bc8f108b1928259b3e539a21c
wxh82138629 <821358629@qq.com>
2019-01-06 22:22:28 +0800

    注册

--- application/index/Controller/Index.php
+++ application/index/Controller/Index.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use app\index\model\User as UserModel;
+
 
 
 class Index extends Controller
@@ -11,6 +13,20 @@ class Index extends Controller
   }
   public function add()
   {
+  	if ($_POST['password']!=$_POST['repassword']) {
+  		return "请输入相同密码！";
+  	}
+  	$user=new UserModel();
+  	$user['username']=$_POST['username'];
+  	$user['password']=$_POST['password'];
+  	$user['email']=$_POST['email'];
+  	$user['creat_time']=date('Y-m-d H:i:s');
+  	if($user ->save()){
+  		return "数据插入成功！";
+  	}else{
+  		return "数据插入失败！";
+  	}
+
   	
   }
     
--- application/index/Model/User.php
+++ application/index/Model/User.php
@@ -1,8 +1,9 @@
 <?php
 namespace app\index\model;
 use think\Model;
+
 class User extends Model
 {
-	public $table="user";/*书上说空着行，但试验之后好像不行？是为什么？*/
+	public $table="user";
 
 }
